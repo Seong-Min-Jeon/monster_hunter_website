eed25e7e6add668c99a7f9a2948c527e745de290
unknown <dustin36@naver.com>
2021-04-13 21:51:49 +0900

    commit

---
+++ script/indexScript.js
@@ -0,0 +1,144 @@
+function effect(choice) {
+    if(choice == 1) {
+        var title = document.getElementById("title1");
+        var build = document.getElementById("build1");                    
+        if(title.innerHTML == "대검") {
+            title.innerHTML = "";
+            build.innerHTML = "";
+        } else {
+            title.innerHTML = "대검";                                        
+            build.innerHTML = '아직 등록된 빌드가 없습니다.';
+        }                    
+    } else if(choice == 2) {
+        var title = document.getElementById("title1");
+        var build = document.getElementById("build1");                    
+        if(title.innerHTML == "태도") {
+            title.innerHTML = "";
+            build.innerHTML = "";
+        } else {
+            title.innerHTML = "태도";                                        
+            build.innerHTML = '아직 등록된 빌드가 없습니다.';
+        } 
+    } else if(choice == 3) {
+        var title = document.getElementById("title1");
+        var build = document.getElementById("build1");                    
+        if(title.innerHTML == "한손검") {
+            title.innerHTML = "";
+            build.innerHTML = "";
+        } else {
+            title.innerHTML = "한손검";                                        
+            build.innerHTML = '아직 등록된 빌드가 없습니다.';
+        } 
+    } else if(choice == 4) {
+        var title = document.getElementById("title1");
+        var build = document.getElementById("build1");
+        if(title.innerHTML == "쌍검") {
+            title.innerHTML = "";
+            build.innerHTML = "";
+        } else {
+            title.innerHTML = "쌍검";                                        
+            build.innerHTML = '<a href="build4_1.html">무속성 깡공 쌍검(21.04)</a>';
+        } 
+    } else if(choice == 5) {
+        var title = document.getElementById("title1");
+        var build = document.getElementById("build1");                    
+        if(title.innerHTML == "해머") {
+            title.innerHTML = "";
+            build.innerHTML = "";
+        } else {
+            title.innerHTML = "해머";                                        
+            build.innerHTML = '아직 등록된 빌드가 없습니다.';
+        } 
+    } else if(choice == 6) {
+        var title = document.getElementById("title1");
+        var build = document.getElementById("build1");                    
+        if(title.innerHTML == "수렵피리") {
+            title.innerHTML = "";
+            build.innerHTML = "";
+        } else {
+            title.innerHTML = "수렵피리";                                        
+            build.innerHTML = '<a href="build6_1.html">탱킹 피리(21.04)</a>';
+        } 
+    } else if(choice == 7) {
+        var title = document.getElementById("title1");
+        var build = document.getElementById("build1");                    
+        if(title.innerHTML == "랜스") {
+            title.innerHTML = "";
+            build.innerHTML = "";
+        } else {
+            title.innerHTML = "랜스";                                        
+            build.innerHTML = '아직 등록된 빌드가 없습니다.';
+        } 
+    } else if(choice == 8) {
+        var title = document.getElementById("title2");
+        var build = document.getElementById("build2");                    
+        if(title.innerHTML == "건랜스") {
+            title.innerHTML = "";
+            build.innerHTML = "";
+        } else {
+            title.innerHTML = "건랜스";                                        
+            build.innerHTML = '아직 등록된 빌드가 없습니다.';
+        } 
+    } else if(choice == 9) {
+        var title = document.getElementById("title2");
+        var build = document.getElementById("build2");                    
+        if(title.innerHTML == "슬래시액스") {
+            title.innerHTML = "";
+            build.innerHTML = "";
+        } else {
+            title.innerHTML = "슬래시액스";                                        
+            build.innerHTML = '아직 등록된 빌드가 없습니다.';
+        } 
+    } else if(choice == 10) {
+        var title = document.getElementById("title2");
+        var build = document.getElementById("build2");                    
+        if(title.innerHTML == "차지액스") {
+            title.innerHTML = "";
+            build.innerHTML = "";
+        } else {
+            title.innerHTML = "차지액스";                                        
+            build.innerHTML = '아직 등록된 빌드가 없습니다.';
+        }
+    } else if(choice == 11) {
+        var title = document.getElementById("title2");
+        var build = document.getElementById("build2");                    
+        if(title.innerHTML == "조충곤") {
+            title.innerHTML = "";
+            build.innerHTML = "";
+        } else {
+            title.innerHTML = "조충곤";                                        
+            build.innerHTML = '아직 등록된 빌드가 없습니다.';
+        }
+    } else if(choice == 12) {
+        var title = document.getElementById("title2");
+        var build = document.getElementById("build2");                    
+        if(title.innerHTML == "활") {
+            title.innerHTML = "";
+            build.innerHTML = "";
+        } else {
+            title.innerHTML = "활";                                        
+            build.innerHTML = '아직 등록된 빌드가 없습니다.';
+        }
+    } else if(choice == 13) {
+        var title = document.getElementById("title2");
+        var build = document.getElementById("build2");                    
+        if(title.innerHTML == "라이트보우건") {
+            title.innerHTML = "";
+            build.innerHTML = "";
+        } else {
+            title.innerHTML = "라이트보우건";                                        
+            build.innerHTML = '아직 등록된 빌드가 없습니다.';
+        }
+    } else if(choice == 14) {
+        var title = document.getElementById("title2");
+        var build = document.getElementById("build2");                    
+        if(title.innerHTML == "헤비보우건") {
+            title.innerHTML = "";
+            build.innerHTML = "";
+        } else {
+            title.innerHTML = "헤비보우건";                                        
+            build.innerHTML = '아직 등록된 빌드가 없습니다.';
+        }
+    }
+   
+} 
